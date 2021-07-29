09434bf3d1ddb64b5f8baee6556948f99cefecf6
DanielNeves <dbenditoneves@gmail.com>
2021-07-29 00:08:45 -0300

    Feito textura para paredes no spritesheet e colocado no jogo, também mudando cor de fundo

--- src/Spritesheet.java
+++ src/Spritesheet.java
@@ -7,6 +7,7 @@ import javax.imageio.ImageIO;
 public class Spritesheet {
     public static BufferedImage spritesheet;
     public static BufferedImage player_front;
+    public static BufferedImage tileWall;
 
     public Spritesheet() {
         try {
@@ -16,6 +17,7 @@ public class Spritesheet {
         }
 
         player_front = Spritesheet.getSprite(0, 11, 16, 16);
+        tileWall = Spritesheet.getSprite(280, 221, 16, 16);
     }
 
     public static BufferedImage getSprite(int x, int y, int width, int height) {
