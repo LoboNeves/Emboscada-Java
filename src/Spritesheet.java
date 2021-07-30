5c760d4a9b87c3900c8913685a148dc9148e5308
DanielNeves <dbenditoneves@gmail.com>
2021-07-29 22:57:33 -0300

    Feito classe bullet e animação de tiro no eixo x

--- src/Spritesheet.java
+++ src/Spritesheet.java
@@ -17,7 +17,7 @@ public class Spritesheet {
         }
 
         player_front = new BufferedImage[2];
-        player_front[0] = Spritesheet.getSprite(1, 11, 16, 16);
+        player_front[0] = Spritesheet.getSprite(0, 11, 16, 16);
         player_front[1] = Spritesheet.getSprite(16, 11, 16, 16);
 
         tileWall = Spritesheet.getSprite(280, 221, 16, 16);
