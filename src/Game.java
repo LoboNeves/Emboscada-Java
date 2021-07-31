80c0f3d70277acf69d055bc4154def65e491c8fd
DanielNeves <dbenditoneves@gmail.com>
2021-07-30 21:16:10 -0300

    IA dos inimigos para peseguir player e sprites para movimento deles em todas as direções

--- src/Game.java
+++ src/Game.java
@@ -16,7 +16,7 @@ public class Game extends Canvas implements Runnable, KeyListener{
     
     public static int WIDTH = 640, HEIGHT = 480;
     public static int SCALE = 3;
-    public Player player;
+    public static Player player;
 
     public World world;
 
@@ -31,6 +31,7 @@ public class Game extends Canvas implements Runnable, KeyListener{
 
         player = new Player(32, 32);
 
+        enemys.add(new Enemy(32, 32));
         enemys.add(new Enemy(32, 32));
     }
 
